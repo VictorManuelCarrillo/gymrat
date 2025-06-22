cb638eb7d0d3d5a7ae70872aa028bdf4505c21d9
Victor Manuel Carrillo <gatitto079@gmail.com>
2025-06-22 04:43:16 -0600

    refactor(Button): :bug: Se comenta codigo

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,20 +5,20 @@ interface ButtonProps {
   color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
   component?: React.ReactElement;
   disable?: boolean;
-  disableElevation?: boolean;
-  disableFocusRipple?: boolean;
-  disableRipple?: boolean;
-  endIcon?: React.ReactNode;
-  fullWidth?: boolean;
-  href?: string;
-  loading?: boolean;
-  loadingIndicator?: React.ReactNode;
-  loadingPosition?: 'center' | 'end' | 'start';
+  // disableElevation?: boolean;
+  // disableFocusRipple?: boolean;
+  // disableRipple?: boolean;
+  // endIcon?: React.ReactNode;
+  // fullWidth?: boolean;
+  // href?: string;
+  // loading?: boolean;
+  // loadingIndicator?: React.ReactNode;
+  // loadingPosition?: 'center' | 'end' | 'start';
 
-  size?: 'small' | 'medium' | 'large';
+  // size?: 'small' | 'medium' | 'large';
 
-  startIcon?: React.ReactNode;
-  variant?: 'contained' | 'outlined' | 'text';
+  // startIcon?: React.ReactNode;
+  // variant?: 'contained' | 'outlined' | 'text';
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
