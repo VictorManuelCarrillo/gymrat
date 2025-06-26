9fae39afe639ce8d573df48834b9103e24bf48ca
kosti846 <reguetton456@gmail.com>
2025-06-26 13:41:42 -0600

    feat: se agregó el componente Button
    Incluye tipado de props: children, color, onClick, href, variant.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
+'use client';
 import { Button } from '~/components/Button';
 
 export default function Home() {
+  const handleClick = () => {
+    alert('relajate mani');
+  };
+
   return (
     <section>
       <div>
         <Button variant="contained">hola</Button>
+        <Button variant="text">hola</Button>
+        <Button variant="outlined">hola</Button>
+        <Button variant="contained" color="error">
+          hola
+        </Button>
+        <Button variant="contained" onClick={handleClick} color="info">
+          hola
+        </Button>
+        <Button variant="contained" onClick={handleClick} color="inherit">
+          hola
+        </Button>
+        <Button variant="contained" onClick={handleClick} color="primary">
+          hola
+        </Button>
+        <Button variant="contained" onClick={handleClick} color="secondary">
+          hola
+        </Button>
+        <Button
+          href="https://forums.spacebattles.com/threads/miraculous-ninja-tales-of-heroes-and-magic-miraculous-ladybug-rc9gn-crossover.976879/#post-80124988"
+          variant="contained"
+          //onClick={handleClick}
+          color="success"
+        >
+          hola
+        </Button>
+        <Button variant="contained" onClick={handleClick} color="warning">
+          hola
+        </Button>
       </div>
     </section>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,28 +1,55 @@
+'use client';
 import { Button as MuiButton } from '@mui/material';
 
+import Style from './button.module.css';
 interface ButtonProps {
   children: React.ReactNode;
   color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
-  component?: React.ReactElement;
-  disable?: boolean;
-  // disableElevation?: boolean;
-  // disableFocusRipple?: boolean;
-  // disableRipple?: boolean;
-  // endIcon?: React.ReactNode;
-  // fullWidth?: boolean;
-  // href?: string;
-  // loading?: boolean;
-  // loadingIndicator?: React.ReactNode;
-  // loadingPosition?: 'center' | 'end' | 'start';
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  //component?: React.ReactElement;
+  //disable?: boolean;
+  //disableElevation?: boolean;
+  //disableFocusRipple?: boolean;
+  //disableRipple?: boolean;
+  //endIcon?: React.ReactNode;
+  //fullWidth?: boolean;
+  href?: string;
+  //loading?: boolean;
+  //loadingIndicator?: React.ReactNode;
+  //loadingPosition?: 'center' | 'end' | 'start';
 
-  // size?: 'small' | 'medium' | 'large';
+  //size?: 'small' | 'medium' | 'large';
 
-  // startIcon?: React.ReactNode;
-  // variant?: 'contained' | 'outlined' | 'text';
+  //startIcon?: React.ReactNode;
+  variant?: 'contained' | 'outlined' | 'text';
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const { children, ...restProps } = props;
+  const { onClick, variant, children, ...restProps } = props;
 
-  return <MuiButton {...restProps}>{children}</MuiButton>;
+  switch (variant) {
+    case 'outlined':
+      return (
+        <MuiButton className={Style.border} onClick={onClick} {...restProps}>
+          {children}
+        </MuiButton>
+      );
+    case 'contained':
+      return (
+        <MuiButton className={Style.outlined} onClick={onClick} {...restProps}>
+          {children}
+        </MuiButton>
+      );
+    case 'text':
+      return (
+        <MuiButton className={Style.text} onClick={onClick} {...restProps}>
+          {children}
+        </MuiButton>
+      );
+      return (
+        <MuiButton variant={variant} className={Style.border} onClick={onClick} {...restProps}>
+          {children}
+        </MuiButton>
+      );
+  }
 };
